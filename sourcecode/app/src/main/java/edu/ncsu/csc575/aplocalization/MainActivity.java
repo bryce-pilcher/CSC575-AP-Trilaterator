2a9b757cb755c96add0c45331e0a452c93788a90
Bryce Pilcher <bcpilche@ncsu.edu>
2016-03-28 12:41:56 -0400

    -Request permission to scan wifi networks.

--- sourcecode/app/src/main/java/edu/ncsu/csc575/aplocalization/MainActivity.java
+++ sourcecode/app/src/main/java/edu/ncsu/csc575/aplocalization/MainActivity.java
@@ -1,5 +1,13 @@
 package edu.ncsu.csc575.aplocalization;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +16,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean wifiPermission = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        String data = "No request";
+
+        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+
+        TextView textView = (TextView) findViewById(R.id.rssi_value);
+        textView.setText(data);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +68,55 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    wifiPermission = true;
+                    getAPs();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    public void getAPs(){
+        String data = "No WiFi Permission";
+        if(wifiPermission) {
+            WifiManager wifiMan = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+            class WifiScanReceiver extends BroadcastReceiver {
+                public void onReceive(Context c, Intent intent) {
+                }
+            }
+            WifiScanReceiver wifiReciever = new WifiScanReceiver();
+            registerReceiver(wifiReciever, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+            List<ScanResult> wifiScanList = wifiMan.getScanResults();
+            if(wifiScanList.size() > 0) {
+                data = wifiScanList.get(0).toString();
+            }
+            else{
+                data = "No APs in Range";
+            }
+        }
+
+        TextView textView = (TextView) findViewById(R.id.rssi_value);
+        textView.setText(data);
     }
 }
